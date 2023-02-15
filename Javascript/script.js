19154e660aaae711d6d09ad917c1162b6814c6c2
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-02-15 23:02:35 +0530

    Variables

---
+++ Javascript/script.js
@@ -0,0 +1,30 @@
+// let variableName = 'value';
+let nithin = 'amazing';
+// nithin => variableName
+// = -> assignment
+
+let countOne = 40;
+console.log(countOne);
+
+let countTwo = 80; //number 
+console.log(countTwo);
+
+let totalCount = countOne + countTwo;
+console.log(totalCount)
+
+let a = 4;
+// let eioewihibvureburobvuorruo = 5;
+
+let hello = "Hello I am Nishtha12334445 ^7***, 456898765"; //string
+
+let hello_world = "hello world!";
+
+let helloWorldEveryone = "Hello World, Everyone!";
+
+// let myVariable = false; //boolean => true and false
+
+// let hello world = "Not possible";
+
+let myVariable;
+console.log(typeof myVariable); 
+
