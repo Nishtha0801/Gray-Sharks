010446a1852300d2b9cef77afbf2d701dbee9c5b
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-02-17 21:48:40 +0530

    let, const and var

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,51 +1,100 @@
-// let variableName = 'value';
-let nithin = 'amazing';
-// nithin => variableName
-// = -> assignment
+// // let variableName = 'value';
+// let nithin = 'amazing';
+// // nithin => variableName
+// // = -> assignment
 
-var countOne = 40;
-console.log(countOne);
+// var countOne = 40;
+// console.log(countOne);
 
-var countTwo = 80; //number 
-console.log(countTwo);
+// var countTwo = 80; //number 
+// console.log(countTwo);
 
-var totalCount = countOne + countTwo;
-console.log(totalCount)
+// var totalCount = countOne + countTwo;
+// console.log(totalCount)
 
-let a = 4;
-// let eioewihibvureburobvuorruo = 5;
+// let a = 4;
+// // let eioewihibvureburobvuorruo = 5;
 
-let hello = "Hello I am Nishtha12334445 ^7***, 456898765"; //string
+// let hello = "Hello I am Nishtha12334445 ^7***, 456898765"; //string
 
-let hello_world = "hello world!";
+// let hello_world = "hello world!";
 
-let helloWorldEveryone = "Hello World, Everyone!";
+// let helloWorldEveryone = "Hello World, Everyone!";
 
-// let myVariable = false; //boolean => true and false
+// // let myVariable = false; //boolean => true and false
 
-// let hello world = "Not possible";
+// // let hello world = "Not possible";
 
-let myVariable;
-console.log(typeof myVariable); // => undefined
+// let myVariable;
+// console.log(typeof myVariable); // => undefined
 
-let myNullVariable = null;
-console.log(typeof myNullVariable); // => object
+// let myNullVariable = null;
+// console.log(typeof myNullVariable); // => object
 
-// key:value
+// // key:value
 
-let myObject = {
-    firstName : "Nishtha",
-    lastName : "Goyal",
-    age:"21",
-}
+// let myObject = {
+//     firstName : "Nishtha",
+//     lastName : "Goyal",
+//     age:"21",
+// }
 
-// property  value  firstName   Nishtha
+// // property  value  firstName   Nishtha
 
-console.log(myObject.firstName);
+// console.log(myObject.firstName);
 
-const myConstantVariable = "Hey, I am a constant variable ki value";
-console.log(myConstantVariable);
+// const ============================================
+
+// const myConstantVariable = "Hey, I am a constant variable ki value";
+// console.log(myConstantVariable);
 
 //  myConstantVariable = "Hey, I am changed";  => error
 // const cannot be updated, const cannot be redeclared.
 
+// let, var, const => we can use these keywords to declare a variable.
+
+//var========================================================
+
+// var myName = "Nishtha";
+
+// // function newFunction(){
+// //     var myLastName = "goyal";
+// //     console.log(myLastName);
+// // }
+
+// // console.log(myName);
+
+// // newFunction();
+
+// // var -> globally and locally/function scoped
+// // var variable can be redeclared and updated.
+
+// var myName = "Nishtha Goyal";
+// console.log(myName);
+
+// myName = "Nishtha Pankaj Goyal"
+// console.log(myName);
+
+// let ==========================================
+
+// let is more prferred for variable declaration
+// let is block scoped {}
+// A block is a piece of code bounded by {}.
+// Anything within these curly braces is a block.
+
+// let myCount = 4;
+
+// if(myCount > 3){
+//     let say = "Say hello!";
+//     console.log(say);
+// }
+
+// console.log(myCount);
+
+// let can be updated but it can't be redeclared
+
+let myName = "Nishtha";
+console.log(myName);
+
+ myName = "Nishtha Goyal";
+ console.log(myName);
