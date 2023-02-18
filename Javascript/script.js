4495e943a2f29c0d283f4e28ddbce9162a6ab6e6
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-02-18 15:38:31 +0530

    operators

--- Javascript/script.js
+++ Javascript/script.js
@@ -160,23 +160,23 @@ const x=5, y=2;
 // }
 
 
-//team 1
-let dol = {
-    s1 : 96,
-    s2 : 108,
-    s3 : 89,
-}
+// //team 1
+// let dol = {
+//     s1 : 96,
+//     s2 : 108,
+//     s3 : 89,
+// }
 
-//team 2
-let koa = {
-    s1 : 88,
-    s2 : 91,
-    s3 : 110,
-}
+// //team 2
+// let koa = {
+//     s1 : 88,
+//     s2 : 91,
+//     s3 : 110,
+// }
 
-// point 1
-let dAverage = (dol.s1 + dol.s2 + dol.s3)/3;
-let kAverage = (koa.s1 + koa.s2 + koa.s3)/3;
+// // point 1
+// let dAverage = (dol.s1 + dol.s2 + dol.s3)/3;
+// let kAverage = (koa.s1 + koa.s2 + koa.s3)/3;
 
 // //point 2
 // if(dAverage > kAverage){
@@ -190,17 +190,84 @@ let kAverage = (koa.s1 + koa.s2 + koa.s3)/3;
 
 // point 3
 // D ko jeetana h
-if(dAverage > kAverage && dAverage >= 100){
-    console.log("D is the winner");
-} else if (kAverage > dAverage && kAverage >= 100){
-    console.log("K is the winner");
+// if(dAverage > kAverage && dAverage >= 100){
+//     console.log("D is the winner");
+// } else if (kAverage > dAverage && kAverage >= 100){
+//     console.log("K is the winner");
+// } else {
+//     // point 4
+//    if(dAverage === kAverage && dAverage>=100 && kAverage>=100){
+//     console.log("Draw");
+//    } else {
+//     console.log("No one wins the trophy");
+//    }
+// }
+
+
+// let country1 = {
+//     countryName: "India",
+//     population: 5000000,
+//     isIsland: false,
+//     language: "English", 
+// }
+
+
+// let country2 = {
+//     countryName: "Finland",
+//     population: 89877,
+//     isIsland: true,
+//     language: "English", 
+// }
+
+// if(country1.population >= 5000000 && country1.language === "English" && !country1.isIsland){
+//     console.log("Perfect country = country1" + country1.countryName);
+// }
+
+
+// Question 1========================================
+let a = 15;
+let b = 15, c = 11;   
+
+if(a > b && a > c){
+    console.log("a is greater");
+} else if(b > a && b > c){
+    console.log("b is greater");
+} else if(c > a && c > b){
+    console.log("c is greater");
 } else {
-    // point 4
-   if(dAverage === kAverage && dAverage>=100 && kAverage>=100){
-    console.log("Draw");
-   } else {
-    console.log("No one wins the trophy");
-   }
+    // homework!
 }
+
+// Question 2 ======================================
+
+let range = {
+    highest: 1000,
+    lowest: 900,   //900 - 1000
+}
+
+let number = 910;
+
+if(number >= range.lowest && number <= range.highest){
+    console.log("Number lying in range!");
+} else {
+    console.log("Not in range");
+}
+
+// Question 3 ===========================================
+
+let numberTocheck = 10;
+
+if(numberTocheck % 2 === 0){
+    console.log("even number");
+} else {
+    console.log("odd numver");
+}
+
+// Ternary Operator/ Conditional Operator
+
+numberTocheck % 2 === 0 ? console.log("my number is even") : console.log("my number is odd");
+// condition ? "Do something, if your condition is satisfied" : otherwise, do this
+
+(number >= range.lowest) && (number <= range.highest) ? console.log("My number is in range") : console.log("My number is out of range");
 
 
