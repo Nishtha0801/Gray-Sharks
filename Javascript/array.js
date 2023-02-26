24e43d17150ee24e9e123761d0113f3f87c8acc4
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-02-26 11:34:49 +0530

    Arrays

--- Javascript/array.js
+++ Javascript/array.js
@@ -1,17 +1,35 @@
-// array of strings
-const words = ["hello", "I", "Am", "Nishtha"];
-console.log(typeof words);
+// // array of strings
+// const words = ["hello", "I", "Am", "Nishtha"];
+// console.log(typeof words);
 
-// empty array
-const myEmptyArray = [];
-console.log(myEmptyArray);
+// // empty array
+// const myEmptyArray = [];
+// console.log(myEmptyArray);
 
-// array of numbers
+// // array of numbers
+// const numbers = [3,4,5,6,7,7,8,9];
+// console.log(numbers);
 
-const numbers = [3,4,5,6,7,7,8,9];
-console.log(numbers);
+// // array with mixed datatypes
+// // array is treated like an object in js.
+// const arrayMixedData = ["hey", 23, false]; //
+// console.log(arrayMixedData);
 
-// array with mixed datatypes
-// array is treated like an object in js.
-const arrayMixedData = ["hey", 23, false];
-console.log(arrayMixedData);
+// const data = [
+//     {'myName' : 'Nishtha'},
+//     [1,2,3,4],
+//     function helloWorld(){
+//         console.log("hello world");
+//     } 
+// ];
+
+// console.log(data);
+
+            //   0 1 2 ..........
+const myArray = [1,2,3,4,5,6,'hello', 'I', 'am', 'Nishtha', true];
+
+console.log(myArray[0]);
+console.log(myArray[10]);
+console.log(myArray[7]);
+console.log(myArray[11]);
+
