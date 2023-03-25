73ac6fbc00df34b7d2b346e6b3ed0e6b4e25838b
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-03-25 14:21:30 +0530

    DOM

--- DOM/practice/practice.js
+++ DOM/practice/practice.js
@@ -44,3 +44,28 @@ add.addEventListener("click", () => {
 
 console.log(list);
 
+let div1 = document.querySelector(".div1");
+console.log(div1);
+
+let isSquare = true;
+
+div1.addEventListener("click", ()=> {
+    if(isSquare){
+        div1.style.borderRadius = "50%";
+        isSquare = false;
+    } else {
+        div1.style.borderRadius = "0%";
+        isSquare = true;
+    }
+});
+
+let fruits = ["apple", "mango", "banana"];
+
+let fruitList = document.getElementById("fruitsList");
+
+fruits.forEach((fruit) => {
+    let itemEle = document.createElement("li");
+    itemEle.textContent = fruit;
+    fruitList.appendChild(itemEle);
+});
+
