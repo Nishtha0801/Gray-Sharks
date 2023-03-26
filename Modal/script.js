be65bd929aeff89a45fb6d2dc59c2eba0abdf8b5
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-03-26 22:41:26 +0530

    modal

---
+++ Modal/script.js
@@ -0,0 +1,19 @@
+let open = document.querySelector(".btn");
+console.log(open);
+
+let content = document.querySelector(".content");
+console.log(content);
+
+let close  = document.querySelector(".close");
+console.log(close);
+
+open.addEventListener("click", ()=>{
+    console.log("button clicked");
+    content.style.display = "flex";
+
+
+});
+
+close.addEventListener("click", () => {
+    content.style.display = "none";
+});
