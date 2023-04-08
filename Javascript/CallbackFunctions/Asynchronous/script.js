6b5c0d4a400054ad6dc546cae03653c990ae37d5
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-04-08 12:07:32 +0530

    callback

--- Javascript/CallbackFunctions/Asynchronous/script.js
+++ Javascript/CallbackFunctions/Asynchronous/script.js
@@ -1,7 +1,32 @@
-console.log("Start");
+// setTimeout()
+// console.log("Start");
 
-setTimeout(() => {
-    console.log("This is set timeout function");
-}, 0);
+// setTimeout(() => {
+//     console.log("This is set timeout function 1");
+// }, 4000);
 
-console.log("End");
+// setTimeout(() => {
+//     console.log("This is set timeout function 2");
+// }, 4000);
+
+// setTimeout(() => {
+//     console.log("This is set timeout function 3");
+// }, 4000);
+
+// console.log("End");
+
+
+// infinite loop ==========================================================
+// let a = true;
+
+// setTimeout(function(){
+//     a = false;
+// }, 10000);
+
+// while(a){
+//     console.log("hello");
+// }
+
+
+// setInterval() =============================================
+
