6280a4dfc990c2aa39107bd6cab6e0bf3354a8b1
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-04-08 14:49:03 +0530

    polyfill

--- Javascript/CallbackFunctions/HighOrderFunctions/script.js
+++ Javascript/CallbackFunctions/HighOrderFunctions/script.js
@@ -1,9 +1,47 @@
 let arr = [1,2,3,4,5,6,7,8,9];
 
 // map
-let newArr = arr.map(item => item * 2);
-console.log(newArr);
+// let newArr = arr.map(item => item * 2);
+// console.log(newArr);
 
 // filter
-let filteredArr = arr.filter(item => item % 2 === 0);
-console.log(filteredArr);
+// let filteredArr = arr.filter(item => item % 2 === 0);
+// console.log(filteredArr);
+
+// reduce
+
+// polyfills ===================================================
+
+// map
+function double(x){
+    return 2*x;
+}
+
+Array.prototype.myMap = function(cb) {
+    let newArr = [];
+    for(let i=0;i<arr.length;i++){
+        newArr.push(cb(arr[i]));
+    }
+    return newArr;
+};
+
+let newArr = arr.myMap(double);
+console.log(newArr);
+
+
+// filter
+function isOdd(x){
+    return x%2 != 0;
+}
+
+Array.prototype.myFilter = function(cb) {
+    let ans = [];
+    for(let i=0;i<arr.length;i++){
+        if(cb(arr[i])){
+            ans.push(arr[i]);
+        }
+    }
+    return ans;
+};
+
+console.log(arr.myFilter(isOdd));
