65caadd65074011fde734749386e1266d4804997
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-04-15 14:34:20 +0530

    promises

--- Javascript/Promises/promise.js
+++ Javascript/Promises/promise.js
@@ -8,23 +8,66 @@
 //         reject(error);
 //     }
 // });
+// let success = false;
 
-let getData = () => {
-    // db call to get the data of the user from databse => asynchronous operation
+
+// let getData = () => {
+//     // db call to get the data of the user from databse => asynchronous operation
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if(success){
+//                 resolve([
+//                     {fname:'nishtha', age: 21},
+//                     {fname:'john', age: 22},
+//                 ]);
+//             } else {
+//                 reject('Failed to get the data');
+//             }    
+//         }, 1000);
+//     });
+// }
+
+// function onFulfilled(data){
+//     console.log(data);
+// }
+
+// function onRejection(error){
+//     console.log(error);
+// }
+
+// let promise = getData();
+// console.log(promise);
+// promise.then(onFulfilled, onRejection);
+
+let func = () => {
+    console.log("You have to call me, whether your promise is resolved or rejected");
+}
+
+function add(a,b){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve([
-                {fname:'nishtha', age: 21},
-                {fname:'john', age: 22},
-            ])
+            if(a === undefined || b === undefined){
+                reject('Please give all the required parameters')
+            } else {
+                resolve(a+b);
+            }
         }, 1000);
     });
 }
 
-function display(data){
-    console.log(data);
-}
 
-let promise = getData();
-console.log(promise);
-promise.then(data => display(data));
+let promise = add(5,7);
+promise.then(value => console.log(value))
+.catch((error) => console.log(error))
+.finally(() => {
+    func();
+});
+
+
+// promise.catch(onRejected);
+
+// promise.catch((error) => {
+//     console.log(error);
+// })
+
+
